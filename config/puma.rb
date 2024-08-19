b137eacf3b3ec9a5b35d5aaee334c764356bb4bd
root <root@prometheus1.servicios.bbr>
2024-08-19 18:10:40 -0400

    daemonize eneable

--- config/puma.rb
+++ config/puma.rb
@@ -15,9 +15,26 @@ pidfile 'var/puma.pid'
 # used by “pumactl” to query and control the server.
 state_path 'var/puma.state'
 
+daemonize
+
 # Disable request logging.
 # The default is “false”.
-quiet
+log_requests
+quiet 
+
+stdout_redirect 'log/stdout.log'
+
+lowlevel_error_handler do |e, env, status|
+  if status == 400
+    message = "The server could not process the request due to an error, such as an incorrectly typed URL, malformed syntax, or a URL that contains illegal characters.\n"
+  else
+    message = "An error has occurred, and engineers have been informed. Please reload the page. If you continue to have problems, contact support@example.com\n"
+    Rollbar.critical(e)
+  end
+
+  [status, {}, [message]]
+end
+
 
 # see more:
 # https://stackoverflow.com/questions/19946153/how-do-i-use-pumas-configuration-file
